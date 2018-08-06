93e17849613262d16de64ef7e6e66f1c9088a611
Alex Weis <alex.weis25@gmail.com>
2018-08-06 16:48:34 -0400

    updates

--- dataStructures.js
+++ dataStructures.js
@@ -147,6 +147,12 @@ console.log('balancedBrackets answer: ', balancedBrackets(s));
 
 /* Asteroids (stacks) */
 
+/* Question
+For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+*/
+
 const asteroids = [-5, 10, 15, -10, 2, -8];
 
 const asteroidCollision = asteroids => {
